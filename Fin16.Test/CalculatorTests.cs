18eaf4f234d9c4e4168f82d1fe34e45db2d97883
kamin0 <jm187@yandex.ru>
2022-10-20 07:35:09 +0300

    Добавьте файлы проекта.

---
+++ Fin16.Test/CalculatorTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+
+
+
+namespace Fin16.Test
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+
+            Assert.AreEqual(6, calculator.Additional(2, 4));
+        }
+
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+
+            Assert.That(calculator.Subtraction(7, 2) == 5);
+        }
+
+        [Test]
+        public void MiltiplicationMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+
+            Assert.AreEqual(6, calculator.Miltiplication(2, 3));
+        }
+
+        [Test]
+        public void DivisionByZero()
+        {
+            var calculator = new Calculator();
+
+            Assert.Throws<DivideByZeroException>(() => calculator.Division(8, 0));
+        }
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+
+            Assert.AreEqual(4, calculator.Division(8, 2));
+        }
+    }
+}
